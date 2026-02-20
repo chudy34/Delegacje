7c4c57a5536fac404bc9ea9f740ccd6b9d123d94
Daniel Jasiński <78660704+chudy34@users.noreply.github.com>
2026-02-20 17:34:43 +0000

    refactor: clean install of system architecture

--- database/init.sql
+++
@@ -1,9 +0,0 @@
--- Delegacje SaaS - Database initialization
--- This runs once when PostgreSQL container first starts
--- Prisma migrations handle the actual schema
-
--- Create extensions
-CREATE EXTENSION IF NOT EXISTS "pg_crypto";
-
--- These will be created by Prisma migrations
--- This file can be used for initial setup steps
